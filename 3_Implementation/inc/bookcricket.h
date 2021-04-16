691d55d31e40432758f056d76587fd70c1b607b5
Chaitanya Kanagala <43912509+Chaitu2134@users.noreply.github.com>
2021-04-16 13:45:17 +0530

    Update bookcricket.h

--- 3_Implementation/inc/bookcricket.h
+++ 3_Implementation/inc/bookcricket.h
@@ -1,12 +1,6 @@
 #ifdef __BOOKCRICKET_H__
 #define __BOOKCRICKET_H__
 
-typedef enum error_t {
-    SUCCESS = 1,
-    ERROR =0,
-    ERROR_DIV_0 = -1
-   }error_t;
-
 int GetPlayerToss(char player_1[],char player_2[],int oversToplay);
 
 int batting(char battingplayer[],char bowlingplayer[],int oversToplay);
